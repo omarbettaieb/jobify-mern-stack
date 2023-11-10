67593d3c3d4d001f618aad0a5dd935452dd15dea
omar <omarbettaieb3@gmail.com>
2023-11-10 14:46:40 +0100

    fronted jobify

---
+++ src/pages/Register.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import {Link} from'react-router-dom'
+import Logo from '../components/Logo'
+import FormRow from '../components/FormRow'
+import Wrapper from '../assets/wrappers/RegisterAndLoginPage'
+
+
+const Register = () => {
+  return (
+    <Wrapper>
+      <form className='form'>
+        <Logo/>
+        <h4>Register</h4>
+        <FormRow type='text' name='name' />
+        <FormRow type='text' name='lastName' labelText='last name' />
+        <FormRow type='text' name='location' />
+        <FormRow type='email' name='email' />
+
+        <FormRow type='password' name='password' />
+        <button type ="submit" className='btn btn-block'>Submit</button>
+      <p>
+        Already a member ?
+        <Link to="/login">Login</Link>
+      </p>
+      </form>
+    </Wrapper>
+  )
+}
+
+export default Register
